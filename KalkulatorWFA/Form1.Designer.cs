ce51fe1a1884eb3013091d5e0ac7652b60a49629
Wnek <Wnek@WNEK-PC>
2017-10-08 15:35:03 +0200

    hmmm

--- KalkulatorWFA/Form1.Designer.cs
+++ KalkulatorWFA/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace KalkulatorWFA
 {
-    partial class Form1
+    partial class Kalkulator
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TBx_1 = new System.Windows.Forms.TextBox();
+            this.but_1 = new System.Windows.Forms.Button();
+            this.but_2 = new System.Windows.Forms.Button();
+            this.but_4 = new System.Windows.Forms.Button();
+            this.but_3 = new System.Windows.Forms.Button();
+            this.but_5 = new System.Windows.Forms.Button();
+            this.but_6 = new System.Windows.Forms.Button();
+            this.but_7 = new System.Windows.Forms.Button();
+            this.but_8 = new System.Windows.Forms.Button();
+            this.but_9 = new System.Windows.Forms.Button();
+            this.but_0 = new System.Windows.Forms.Button();
+            this.but_multi = new System.Windows.Forms.Button();
+            this.but_div = new System.Windows.Forms.Button();
+            this.but_subst = new System.Windows.Forms.Button();
+            this.but_add = new System.Windows.Forms.Button();
+            this.but_equal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TBx_1
+            // 
+            this.TBx_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBx_1.Location = new System.Drawing.Point(71, 33);
+            this.TBx_1.Multiline = true;
+            this.TBx_1.Name = "TBx_1";
+            this.TBx_1.Size = new System.Drawing.Size(150, 30);
+            this.TBx_1.TabIndex = 0;
+            this.TBx_1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // but_1
+            // 
+            this.but_1.Location = new System.Drawing.Point(89, 82);
+            this.but_1.Name = "but_1";
+            this.but_1.Size = new System.Drawing.Size(33, 23);
+            this.but_1.TabIndex = 1;
+            this.but_1.Text = "1";
+            this.but_1.UseVisualStyleBackColor = true;
+            this.but_1.Click += new System.EventHandler(this.but_1_Click);
+            // 
+            // but_2
+            // 
+            this.but_2.Location = new System.Drawing.Point(132, 82);
+            this.but_2.Name = "but_2";
+            this.but_2.Size = new System.Drawing.Size(33, 23);
+            this.but_2.TabIndex = 2;
+            this.but_2.Text = "2";
+            this.but_2.UseVisualStyleBackColor = true;
+            this.but_2.Click += new System.EventHandler(this.but_2_Click);
+            // 
+            // but_4
+            // 
+            this.but_4.Location = new System.Drawing.Point(89, 111);
+            this.but_4.Name = "but_4";
+            this.but_4.Size = new System.Drawing.Size(33, 23);
+            this.but_4.TabIndex = 3;
+            this.but_4.Text = "4";
+            this.but_4.UseVisualStyleBackColor = true;
+            this.but_4.Click += new System.EventHandler(this.but_4_Click);
+            // 
+            // but_3
+            // 
+            this.but_3.Location = new System.Drawing.Point(171, 82);
+            this.but_3.Name = "but_3";
+            this.but_3.Size = new System.Drawing.Size(33, 23);
+            this.but_3.TabIndex = 4;
+            this.but_3.Text = "3";
+            this.but_3.UseVisualStyleBackColor = true;
+            this.but_3.Click += new System.EventHandler(this.but_3_Click);
+            // 
+            // but_5
+            // 
+            this.but_5.Location = new System.Drawing.Point(132, 111);
+            this.but_5.Name = "but_5";
+            this.but_5.Size = new System.Drawing.Size(33, 23);
+            this.but_5.TabIndex = 5;
+            this.but_5.Text = "5";
+            this.but_5.UseVisualStyleBackColor = true;
+            this.but_5.Click += new System.EventHandler(this.but_5_Click);
+            // 
+            // but_6
+            // 
+            this.but_6.Location = new System.Drawing.Point(171, 111);
+            this.but_6.Name = "but_6";
+            this.but_6.Size = new System.Drawing.Size(33, 23);
+            this.but_6.TabIndex = 6;
+            this.but_6.Text = "6";
+            this.but_6.UseVisualStyleBackColor = true;
+            this.but_6.Click += new System.EventHandler(this.but_6_Click);
+            // 
+            // but_7
+            // 
+            this.but_7.Location = new System.Drawing.Point(89, 140);
+            this.but_7.Name = "but_7";
+            this.but_7.Size = new System.Drawing.Size(33, 23);
+            this.but_7.TabIndex = 7;
+            this.but_7.Text = "7";
+            this.but_7.UseVisualStyleBackColor = true;
+            this.but_7.Click += new System.EventHandler(this.but_7_Click);
+            // 
+            // but_8
+            // 
+            this.but_8.Location = new System.Drawing.Point(132, 140);
+            this.but_8.Name = "but_8";
+            this.but_8.Size = new System.Drawing.Size(33, 23);
+            this.but_8.TabIndex = 8;
+            this.but_8.Text = "8";
+            this.but_8.UseVisualStyleBackColor = true;
+            this.but_8.Click += new System.EventHandler(this.but_8_Click);
+            // 
+            // but_9
+            // 
+            this.but_9.Location = new System.Drawing.Point(171, 140);
+            this.but_9.Name = "but_9";
+            this.but_9.Size = new System.Drawing.Size(33, 23);
+            this.but_9.TabIndex = 9;
+            this.but_9.Text = "9";
+            this.but_9.UseVisualStyleBackColor = true;
+            this.but_9.Click += new System.EventHandler(this.but_9_Click);
+            // 
+            // but_0
+            // 
+            this.but_0.Location = new System.Drawing.Point(132, 169);
+            this.but_0.Name = "but_0";
+            this.but_0.Size = new System.Drawing.Size(33, 23);
+            this.but_0.TabIndex = 10;
+            this.but_0.Text = "0";
+            this.but_0.UseVisualStyleBackColor = true;
+            this.but_0.Click += new System.EventHandler(this.but_0_Click);
+            // 
+            // but_multi
+            // 
+            this.but_multi.Location = new System.Drawing.Point(89, 169);
+            this.but_multi.Name = "but_multi";
+            this.but_multi.Size = new System.Drawing.Size(33, 23);
+            this.but_multi.TabIndex = 11;
+            this.but_multi.Text = "*";
+            this.but_multi.UseVisualStyleBackColor = true;
+            // 
+            // but_div
+            // 
+            this.but_div.Location = new System.Drawing.Point(171, 169);
+            this.but_div.Name = "but_div";
+            this.but_div.Size = new System.Drawing.Size(33, 23);
+            this.but_div.TabIndex = 12;
+            this.but_div.Text = "/";
+            this.but_div.UseVisualStyleBackColor = true;
+            // 
+            // but_subst
+            // 
+            this.but_subst.Location = new System.Drawing.Point(171, 198);
+            this.but_subst.Name = "but_subst";
+            this.but_subst.Size = new System.Drawing.Size(33, 23);
+            this.but_subst.TabIndex = 13;
+            this.but_subst.Text = "-";
+            this.but_subst.UseVisualStyleBackColor = true;
+            // 
+            // but_add
+            // 
+            this.but_add.Location = new System.Drawing.Point(89, 198);
+            this.but_add.Name = "but_add";
+            this.but_add.Size = new System.Drawing.Size(33, 23);
+            this.but_add.TabIndex = 14;
+            this.but_add.Text = "+";
+            this.but_add.UseVisualStyleBackColor = true;
+            this.but_add.Click += new System.EventHandler(this.but_add_Click);
+            // 
+            // but_equal
+            // 
+            this.but_equal.Location = new System.Drawing.Point(132, 198);
+            this.but_equal.Name = "but_equal";
+            this.but_equal.Size = new System.Drawing.Size(33, 23);
+            this.but_equal.TabIndex = 15;
+            this.but_equal.Text = "=";
+            this.but_equal.UseVisualStyleBackColor = true;
+            // 
+            // Kalkulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(295, 264);
+            this.Controls.Add(this.but_equal);
+            this.Controls.Add(this.but_add);
+            this.Controls.Add(this.but_subst);
+            this.Controls.Add(this.but_div);
+            this.Controls.Add(this.but_multi);
+            this.Controls.Add(this.but_0);
+            this.Controls.Add(this.but_9);
+            this.Controls.Add(this.but_8);
+            this.Controls.Add(this.but_7);
+            this.Controls.Add(this.but_6);
+            this.Controls.Add(this.but_5);
+            this.Controls.Add(this.but_3);
+            this.Controls.Add(this.but_4);
+            this.Controls.Add(this.but_2);
+            this.Controls.Add(this.but_1);
+            this.Controls.Add(this.TBx_1);
+            this.Name = "Kalkulator";
+            this.Text = "Kalkulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TBx_1;
+        private System.Windows.Forms.Button but_1;
+        private System.Windows.Forms.Button but_2;
+        private System.Windows.Forms.Button but_4;
+        private System.Windows.Forms.Button but_3;
+        private System.Windows.Forms.Button but_5;
+        private System.Windows.Forms.Button but_6;
+        private System.Windows.Forms.Button but_7;
+        private System.Windows.Forms.Button but_8;
+        private System.Windows.Forms.Button but_9;
+        private System.Windows.Forms.Button but_0;
+        private System.Windows.Forms.Button but_multi;
+        private System.Windows.Forms.Button but_div;
+        private System.Windows.Forms.Button but_subst;
+        private System.Windows.Forms.Button but_add;
+        private System.Windows.Forms.Button but_equal;
     }
 }
 
--- KalkulatorWFA/Form1.cs
+++ KalkulatorWFA/Form1.cs
@@ -10,11 +10,108 @@ using System.Windows.Forms;
 
 namespace KalkulatorWFA
 {
-    public partial class Form1 : Form
+    public partial class Kalkulator : Form
     {
-        public Form1()
+        long zmienna = 0;
+        private long liczba(long x)
+        {
+            if (x != 0) {
+                zmienna = zmienna * 10 + x;
+               
+            }
+            else
+            {
+                zmienna = zmienna * 10;
+            }
+            return zmienna;
+        }
+        
+        private long add(long x, y)
+        {
+
+            return sum;
+        }
+        public Kalkulator()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        #region buttony 0-9
+        private void but_1_Click(object sender, EventArgs e)
+        {
+            long i = 1;
+            liczba(i);
+            TBx_1.Text = zmienna.ToString();
+        }
+        private void but_2_Click(object sender, EventArgs e)
+        {
+            long i = 2;
+            liczba(i);
+            TBx_1.Text =  zmienna.ToString();
+        }
+        private void but_3_Click(object sender, EventArgs e)
+        {
+            long i = 3;
+            liczba(i);
+            TBx_1.Text = zmienna.ToString();
+        }
+        private void but_4_Click(object sender, EventArgs e)
+        {
+            long i = 4;
+            liczba(i);
+            TBx_1.Text = zmienna.ToString();
+        }
+        private void but_5_Click(object sender, EventArgs e)
+        {
+            long i = 5;
+            liczba(i);
+            TBx_1.Text = zmienna.ToString();
+        }
+        private void but_6_Click(object sender, EventArgs e)
+        {
+            long i = 6;
+            liczba(i);
+            TBx_1.Text = zmienna.ToString();
+        }
+        private void but_7_Click(object sender, EventArgs e)
+        {
+            long i = 7;
+            liczba(i);
+            TBx_1.Text = zmienna.ToString();
+        }
+        private void but_8_Click(object sender, EventArgs e)
+        {
+            long i = 8;
+            liczba(i);
+            TBx_1.Text = zmienna.ToString();
+        }
+        private void but_9_Click(object sender, EventArgs e)
+        {
+            long i = 9;
+            liczba(i);
+            TBx_1.Text = zmienna.ToString();
+        }
+        private void but_0_Click(object sender, EventArgs e)
+        {
+            long i = 0;
+            liczba(i);
+            TBx_1.Text = zmienna.ToString();
+        }
+        #endregion
+
+        private void but_add_Click(object sender, EventArgs e)
+        {
+            TBx_1.Text = "+";
+
         }
     }
 }
